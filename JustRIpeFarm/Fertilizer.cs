cd0e1157b3bc0f80bd850052592732a4bccc8e6c
POPPINCURSE\Marcus <marcuskong015@gmail.com>
2018-11-12 16:25:26 +0800

    modified again the fertilizer file liao :)

--- JustRIpeFarm/Fertilizer.cs
+++ JustRIpeFarm/Fertilizer.cs
@@ -9,15 +9,17 @@ namespace JustRIpeFarm
     //By Miss Asia
     class Fertilizer
     {
-        private int stockId;
+        private int FertId;
         private int quantity;
         private string typeOfFertilizer;
+        private string FertName;
         private DateTime purchaseDate;
         private DateTime expiryDate;
 
+        public string fertName { get => FertName; set => FertName = value; }
         public string Type { get => typeOfFertilizer; set => typeOfFertilizer = value; }
         public int Quantity { get => quantity; set => quantity = value; }
-        public int StockId { get => stockId; set => stockId = value; }
+        public int fertID { get => FertId; set => FertId = value; }
         public DateTime PurchaseDate { get => purchaseDate; set => purchaseDate = value; }
         public DateTime ExpiryDate { get => expiryDate; set => expiryDate = value; }
     }

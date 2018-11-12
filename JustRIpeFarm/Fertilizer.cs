91699317e369cc534686747cb3531c7b886d271c
POPPINCURSE\Marcus <marcuskong015@gmail.com>
2018-11-12 16:20:42 +0800

    add liao fertilizer class file, almost same der. :)

---
+++ JustRIpeFarm/Fertilizer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustRIpeFarm
+{
+    //By Miss Asia
+    class Fertilizer
+    {
+        private int stockId;
+        private int quantity;
+        private string typeOfFertilizer;
+        private DateTime purchaseDate;
+        private DateTime expiryDate;
+
+        public string Type { get => typeOfFertilizer; set => typeOfFertilizer = value; }
+        public int Quantity { get => quantity; set => quantity = value; }
+        public int StockId { get => stockId; set => stockId = value; }
+        public DateTime PurchaseDate { get => purchaseDate; set => purchaseDate = value; }
+        public DateTime ExpiryDate { get => expiryDate; set => expiryDate = value; }
+    }
+}
